718c0852c0acc61505bb6ff2dc04f258e1bc3362
Mikhail Chekov <chekoff@list.ru>
2020-08-12 11:22:14 +0300

    Updates

---
+++ precache-manifest.098be4c273ed9b6bacdae2adff2649f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e5ea29f09c4c29473bdea8608378a9",
+    "url": "/snake-game-reactjs/index.html"
+  },
+  {
+    "revision": "39ed5246f7dbb8399195",
+    "url": "/snake-game-reactjs/static/css/main.cd6e762c.chunk.css"
+  },
+  {
+    "revision": "ca749e734afda3f75d47",
+    "url": "/snake-game-reactjs/static/js/2.91685e74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-game-reactjs/static/js/2.91685e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ed5246f7dbb8399195",
+    "url": "/snake-game-reactjs/static/js/main.2fc8eea9.chunk.js"
+  },
+  {
+    "revision": "1c275bbec8652b075a70",
+    "url": "/snake-game-reactjs/static/js/runtime-main.fa76f4c5.js"
+  },
+  {
+    "revision": "e6b74eac7530163617ee5870a328e7e1",
+    "url": "/snake-game-reactjs/static/media/firework-min.e6b74eac.jpg"
+  }
+]);
